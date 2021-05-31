735ccf8074c34ce612c09e4a3b3c9c9f988c4383
Jacob Cardenas <201520407e@utea.edu.pe>
2021-05-31 10:09:10 -0500

    proyecto culminado

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Leccion: 6 Botones'),
+          ),
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: SafeArea(
+                    child: Text(
+                      'Bienvenido a Android ATC',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                          fontSize: 25.0),
+                    ),
+                  ),
+                ),
+
+                //Raised Button
+                RaisedButton(
+                  color: Colors.blue,
+                  child: Text(
+                    'Raised Button',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                        fontSize: 20.0),
+                  ),
+                  onPressed: () {},
+                ),
+
+                //Icon Button
+                IconButton(
+                  icon: Icon(Icons.volume_up),
+                  tooltip: 'Increase volume by 10',
+                  onPressed: () {},
+                ),
+                Text('Sound'),
+
+                //Flat Button
+                FlatButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  splashColor: Colors.blueAccent,
+                  onPressed: () {},
+                  child: Text(
+                    'Flat Button',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.phone),
+          )),
+    );
+  }
+}
